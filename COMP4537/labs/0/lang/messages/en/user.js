382a450104f47c350e778e4074bf076719f78216
Echo Wang <ywang623@my.bcit.ca>
2025-01-09 17:32:25 -0800

    first commit

---
+++ COMP4537/labs/0/lang/messages/en/user.js
@@ -0,0 +1,5 @@
+export const prompt = "How many buttons to create?";
+export const button = "Go!";
+export const success = "Excellent memory!";
+export const failure = "Wrong order!";
+export const invalidInput = "Please enter a number from 3 to 7";
