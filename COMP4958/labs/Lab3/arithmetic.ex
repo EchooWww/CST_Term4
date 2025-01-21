7db9c9a08516c2a6e7d42c9491e4a76eea806258
Echo Wang <ywang623@my.bcit.ca>
2025-01-20 21:01:31 -0800

    Finish COMP4958 lab3

---
+++ COMP4958/labs/Lab3/arithmetic.ex
@@ -0,0 +1,73 @@
+defmodule Arithmetic.Worker do
+  use GenServer
+  def start() do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def square(pid, x) do
+    GenServer.call(pid, {:square, x})
+  end
+
+  def sqrt(pid, x) do
+    GenServer.call(pid, {:sqrt, x})
+  end
+
+  @impl true
+  def init(arg) do
+    {:ok, arg}
+  end
+
+  @impl true
+  def handle_call({:square, x}, _from, state) do
+    {:reply,{self(), x * x}, state}
+  end
+
+  @impl true
+  def handle_call({:sqrt, x}, _from, state) do
+    Process.sleep(4000)
+    reply = if x >=0, do: :math.sqrt(x), else: :error
+    {:reply, {self(),reply}, state}
+  end
+end
+
+defmodule Arithmetic.Server do
+  use GenServer
+  def start(n\\1)
+    when is_integer(n) and n > 0 do
+      GenServer.start(__MODULE__, n, name: __MODULE__)
+    end
+
+  def square(x) do
+    GenServer.call(__MODULE__, {:square, x})
+  end
+
+  def sqrt(x) do
+    GenServer.call(__MODULE__, {:sqrt, x})
+  end
+
+  @impl true
+  def init(num_workers) do
+    workers =
+      for _ <- 1..num_workers do
+        {:ok, pid} = Arithmetic.Worker.start()
+        IO.puts("Worker started with PID: #{inspect(pid)}")
+        pid
+      end
+    {:ok, %{workers: workers, next: 0}}
+  end
+
+  @impl true
+  def handle_call({operation, x}, from, state) do
+    worker = Enum.at(state.workers, state.next)
+    spawn(fn->
+      result =
+        case operation do
+        :square -> Arithmetic.Worker.square(worker, x)
+        :sqrt -> Arithmetic.Worker.sqrt(worker, x)
+      end
+      GenServer.reply(from, result)
+    end)
+    next = rem(state.next + 1, length(state.workers))
+    {:noreply, %{state | next: next}}
+  end
+end
